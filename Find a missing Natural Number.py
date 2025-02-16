0dce8b32335075c306c808e09590d7c3ad64679a
LuoisVeasna <161813651+LuoisVeasna@users.noreply.github.com>
2025-02-16 19:39:26 +0700

    Add files via upload
    Find a missing Natural Number

---
+++ Find a missing Natural Number.py
@@ -0,0 +1,23 @@
+
+import array
+
+def missing_num(given_list):
+
+    stat = given_list[0]
+    end = given_list[len(given_list) -1 ]
+
+    actual_list = list(range(stat, end +1))
+    actual_sum = sum(actual_list)
+    given_sum = sum(given_list)
+
+    return actual_sum - given_sum
+
+arr = array.array('i',[11,12,13,14,15,16,17])
+
+miss_num = missing_num(arr)
+if miss_num > 0:
+    print('Missing number is', missing_num(arr))
+else:
+    print('No Missing Number Found')
+
+
